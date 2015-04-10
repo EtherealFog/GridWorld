874acc324470a42b83c18c674673dc92215159ac
EtherealFog <baldwinelias77@gmail.com>
2015-04-10 13:59:00 -0700

    slight alterations

--- GridWorld1/src/com/runners/MultiRunner.java
+++ GridWorld1/src/com/runners/MultiRunner.java
@@ -7,10 +7,11 @@ import com.bugs.SpiralBug;
 import com.bugs.ZBug;
 
 public class MultiRunner {
-	public static void main(String args) {	
+	public static void main(String[] args) {	
 		ActorWorld world = new ActorWorld();
 		world.add(new SpiralBug());
 		world.add(new ZBug(5));
-		world.add(new DancingBug(new int[] {3, 5, 5, 2, 7, 9, 12, 3, 5, 3, 1, 1}));
+		world.add(new DancingBug(new int[] {3, 5, 2, 2, 4, 3, 1, 0, 5, 3, 0, 1}));
+		world.show();
 	}
 }
