2c50584b5bcaf2000848c055888110e778670495
EtherealFog <baldwinelias77@gmail.com>
2015-04-28 20:28:32 -0700

    Finished Grub (pretty sure).

--- GridWorld1/src/com/critters/Grub.java
+++ GridWorld1/src/com/critters/Grub.java
@@ -1,6 +1,7 @@
 package com.critters;
 
 import info.gridworld.actor.Critter;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 
 import java.util.ArrayList;
@@ -15,22 +16,29 @@ public class Grub extends Critter {
 	public int getRandomDirection() {
 		int[] directions = {Location.NORTH, Location.NORTHEAST, Location.EAST, Location.SOUTHEAST, 
 							Location.SOUTH, Location.SOUTHWEST, Location.WEST, Location.NORTHWEST};
-		return directions[(int)(Math.random() * 8)];
+		return directions[(int)(Math.random() * directions.length)];
 	}
 	
 	public ArrayList<Location> getMoveLocations() {
 		ArrayList<Location> locs = new ArrayList<Location>();
 		int direction = getRandomDirection();
 		if(maxDistance >= 1) {
-			locs.add();
+			locs.add(getLocation().getAdjacentLocation(direction));
+			for(int i = 1; i < maxDistance; i++) {
+				Location temp = locs.get(locs.size() - 1);
+				locs.add(temp.getAdjacentLocation(direction));
+			}
 		}
 		return locs;
 	}
 	
 	public Location selectMoveLocation(ArrayList<Location> locs) {
 		Location loc = null;
-		
-		
-		return loc;
+		loc = locs.get((int)(Math.random() * locs.size()));
+		if(getGrid().get(loc) instanceof Flower || getGrid().get(loc) == null) {
+			return loc;
+		} else {
+			return null;
+		}
 	}
 }
